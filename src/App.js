2facbe7775b59931afea68a9176db690a1505d48
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-11-05 22:01:50 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Shivam Youtube Clone </h1>
+      {/* <h1>Shivam Youtube Clone </h1> */}
+      <Header></Header>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MenuIcon from '@material-ui/icons/Menu'
+function Header() {
+    return (
+        <div className="header">
+            <MenuIcon/>
+        </div>
+    )
+}
+
+export default Header
