5ab37672258773d97fc6bea36a771d93de4eaa9b
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-11-08 20:50:51 +0530

    first commit

--- src/Header.js
+++ src/Header.js
@@ -9,14 +9,21 @@ import { Avatar } from '@material-ui/core'
 function Header() {
     return (
         <div className="header">
+            <div className="header__left">
             <MenuIcon/>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/YouTube_Logo_%282013-2017%29.svg/2560px-YouTube_Logo_%282013-2017%29.svg.png" alt="yt" className="header_logo"></img>
-            <input type="text" name="" id="" />
-            <SearchIcon/>
+            </div>
+            <div className="header_input">
+                <input type="text" name="" id="" />
+                <SearchIcon/>
+            </div>
+            <div className="header_icons">
             <VideoCallIcon/>
             <AppsIcon/>
             <NotificationsIcon/>
             <Avatar src="https://media-exp1.licdn.com/dms/image/C5603AQEnP4CjCPZcgw/profile-displayphoto-shrink_200_200/0/1600079811803?e=1638403200&v=beta&t=OAyuuI0BNjAHg5-ySkLkF2vKlP86e__AVn8_G-YNWKs"/>
+            </div>
+            
             
         </div>
     )
