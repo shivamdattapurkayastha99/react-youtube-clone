d62e093c19476a33844477ba7227109727417fb0
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-11-24 21:09:46 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       {/* <h1>Shivam Youtube Clone </h1> */}
       <Header></Header>
+      
+
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -14,13 +14,13 @@ function Header() {
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/YouTube_Logo_%282013-2017%29.svg/2560px-YouTube_Logo_%282013-2017%29.svg.png" alt="yt" className="header_logo"></img>
             </div>
             <div className="header_input">
-                <input type="text" name="" id="" />
-                <SearchIcon/>
+                <input type="text" name="" id="" placeholder="Search" />
+                <SearchIcon className="header_inputButton"/>
             </div>
             <div className="header_icons">
-            <VideoCallIcon/>
+            <VideoCallIcon className="header_icon"/>
             <AppsIcon/>
-            <NotificationsIcon/>
+            <NotificationsIcon className="header_icon"/>
             <Avatar src="https://media-exp1.licdn.com/dms/image/C5603AQEnP4CjCPZcgw/profile-displayphoto-shrink_200_200/0/1600079811803?e=1638403200&v=beta&t=OAyuuI0BNjAHg5-ySkLkF2vKlP86e__AVn8_G-YNWKs"/>
             </div>
             
